012697e8adfc5276fbcca0dd354c4f67a66a4581
amoghj8 <amoghj8@gmail.com>
2017-09-29 10:22:28 +0530

    Edit merge sort file

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
-
 using namespace std;
 
-
-void merge(int* ALR, int* L, int left_length, int* R, int right_length) {
-    int l = 0;
-    int r = 0;
-    for (int i = 0; i < left_length + right_length;) {
-
-        if (l == left_length)ALR[i++] = R[r++];
-        else if (r == right_length)ALR[i++] = L[l++];
-        else ALR[i++] = (R[r] > L[l]) ? L[l++] : R[r++];
-
+void merge(int* ALR, int* L, int left_length, int* R, int right_length)
+{
+    int nL = left_length;
+    int nR = right_length;
+    int i=0;int j=0;int k=0;
+    while (i<nL && j<nR) {
+      if(L[i]<=R[j])
+      {
+        ALR[k] = L[i];
+        k++;
+        i++;
+      }
+      else
+      {
+        ALR[k] = R[j];
+        k++;
+        j++;
+      }
+    }
+    while (i<nL)
+    {
+      ALR[k] = L[i];
+      k++;
+      i++;
+    }
+    while (j<nR)
+    {
+      ALR[k] = R[j];
+      k++;
+      j++;
     }
 }
 
-void merge_sort(int* ALR, int length) {
+void MergeSort(int* A, int size)
+{
+    if (size<2)
+      return;
+    int mid = size/2;
+    int* left = new int[mid];
+    int* right = new int[size-mid];
+    for (int i=0;i<mid;i++) {
+      left[i] = A[i];
+    }
+    for (int i=mid;i<size;i++) {
+      right[i-mid] = A[i];
+    }
 
-    if (length == 1)return;
+    MergeSort(left, mid);
+    MergeSort(right, size - mid);
 
-    int mid = length / 2;
-
-    int* L = new int[mid];
-    int* R = new int[length - mid];
-
-    int k = 0;
-
-    for (size_t i = 0; k < mid; i++)L[i] = ALR[k++];
-    for (size_t i = 0; k < length; i++)R[i] = ALR[k++];
-
-    merge_sort(L, mid);
-    merge_sort(R, length - mid);
-
-    merge(ALR, L, mid, R, length - mid);
-    delete(L);
-    delete(R);
+    merge(A, left, mid, right, size - mid);
+    delete(left);
+    delete(right);
 }
+
 int main() {
 
-    int A[] = { 1,3,4,7,2,8,5,6,9 };
+    int A[] = { 1,-3,14,7,52,8,505,-60,9 };
 
     int size = sizeof(A) / sizeof(A[0]);
 
-    merge_sort(A, size);
+    MergeSort(A, size);
 
     cout<<"Sorted array\n";
     for (size_t i = 0; i < size; i++)cout<<A[i]<<" ";
 
-
-
+    return 0;
 }
