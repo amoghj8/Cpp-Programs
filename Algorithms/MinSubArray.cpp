13df55ff735fe7eaf5e24338fad2865e05006cdf
amoghj8 <amoghj8@gmail.com>
2017-10-10 17:00:38 +0530

    Add Min Sub Array implementation

---
+++ Algorithms/MinSubArray.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter the no. of elements\n";
+  cin>>n;
+  int a[n];
+  for (size_t i = 0; i < n; i++) {
+    cin>>a[i];
+  }
+
+
+  int ans = -1*a[0],
+  	ans_l = 0,
+  	ans_r = 0,
+  	sum = 0,
+  	minus_pos = -1;
+  for (int r=0; r<n; ++r) {
+  	sum += -1*a[r];
+
+  	if (sum > ans) {
+  		ans = sum;
+  		ans_l = minus_pos + 1;
+  		ans_r = r;
+  	}
+
+  	if (sum < 0) {
+  		sum = 0;
+  		minus_pos = r;
+  	}
+  }
+  cout<<-1*ans<<endl;
+  cout<<"Max start index\t"<<ans_l<<endl;
+  cout<<"Max end index\t"<<ans_r<<endl;
+}
