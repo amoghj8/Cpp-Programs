5b5eebec07e593de639c02cdcf1a73b55f671478
amoghj8 <amoghj8@gmail.com>
2017-10-17 19:43:56 +0530

    Add GCD implementation

---
+++ Algorithms/Euclid'sGCD.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+
+using namespace std;
+
+int gcd (int a, int b) {
+	if (b == 0)
+		return a;
+	else
+		return gcd (b, a % b);
+}
+
+int main()
+{
+  int a,b;
+  cout<<"Enter two intergers\n";
+  cin>>a>>b;
+  cout<<gcd(a,b)<<"\n";
+  return 0;
+}
